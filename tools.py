e59aaaa6809e09eb7f62dfe132212818a034d8c7
Toletum <toletum@gmail.com>
2025-03-03 20:38:12 +0100

    ollama -> openai, agent, tools

--- tools.py
+++ tools.py
@@ -4,19 +4,16 @@ from phi.tools.duckduckgo import DuckDuckGo
 
 
 model = OpenAIChat(base_url="http://localhost:11434/v1",
-                     api_key='ollama',
-                         id="llama3.2:latest")
+                   api_key='ollama',
+                   model='llama3.2:latest')
 
 
-# Crear el agente con la herramienta
 agente = Agent(
     name="Intelligent Assistant",
     model=model,
     instructions=["You are an intelligent assistant designed to answer questions using the provided tools. When asked a question that requires up-to-date information, use the duckduckgo_search tool to find relevant information from the web. After getting the search results, summarize the answer to the user's question based on the information found."],
     tools=[DuckDuckGo()],
-    show_tool_calls=True,
-    stream = True
+    show_tool_calls=True
 )
 
-# Prueba del agente
 agente.print_response("who is the current POTUS?", markdown=True)
