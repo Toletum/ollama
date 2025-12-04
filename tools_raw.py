954d7157d0efd983f7769088034bf154c116e86a
Toletum <toletum@gmail.com>
2025-12-04 16:57:23 +0100

    Update tools_raw.py

--- tools_raw.py
+++ tools_raw.py
@@ -13,6 +13,18 @@ import ollama
 import requests
 from pydantic import json
 
+"""
+Using tools in ollama from the scratch, no framework
+
+TOOLS:
+    get_time: Returns the current time in ISO 8601 format
+    get_coor: Return in json forma the coordinates for the city
+
+The tools are cached because sometime the call is repeat
+
+
+"""
+
 
 
 @lru_cache(maxsize=1024)
@@ -28,7 +40,7 @@ def get_time(timezone: str) -> json:
         'tz': repr(tz),
         'time': now.strftime('%H:%M:%S')
     }
-
+# ----------------------------------------------------------------------------------
 
 @lru_cache(maxsize=1024)
 def get_coor(city: str) -> json:
@@ -57,8 +69,9 @@ def get_coor(city: str) -> json:
             "city": city,
             "error": repr(ex)
         }
+# ----------------------------------------------------------------------------------
 
-
+# Doc tools
 TOOLS = [
     {
         "type": "function",
@@ -96,48 +109,53 @@ TOOLS = [
     }
 ]
 
-
+#  Initial message: The request
 messages = [
     {
         'role': 'user',
-        'content': "what time is it now in Bilbao localtime, UTC time and latitude/longitude? Please, some info about the city",
+        'content': """what time is it now in Bilbao localtime, UTC time and latitude/longitude? 
+        Please, some info about the city""",
     } ]
 
 
 module = sys.modules[__name__]
 
 loop = 0
-while True:
+while True: #  Until no tools to call
     loop += 1
     print(f"Loop {loop}")
     response = ollama.chat(
         model="llama3.1:latest",
         messages=messages,
-        tools=TOOLS
+        tools=TOOLS #  Register tools
     )
 
+    # History
     messages.append(response['message'])
 
-    if len(response['message'].get('tool_calls', [])) == 0:
-        print(response['message']['content'])
-        break
-
+    if len(response['message'].get('tool_calls', [])) == 0: #  No more tools to call
+        break #  Bye bye
 
     for tool_call in response['message']['tool_calls']:
         function_name = tool_call['function']['name']
         function_args = tool_call['function']['arguments']
         try:
+            calling = time.time()
             print(f"Calling... '{function_name}' args: {function_args}")
             function_call = getattr(module, function_name)
-            calling = time.time()
-            function_response = function_call(**function_args)
-            round(time.time() - calling, 4)
+            response = function_call(**function_args)
+            # Add to the context
             messages.append({
                 "role": "tool",
-                "content": str(function_response),
+                "content": str(response),
                 "tool_call_id": str(uuid.uuid4())
             })
-            print(f"    REQUEST: {round(time.time() - calling, 4)}  DATA: {str(function_response)}")
+            round(time.time() - calling, 4)
+            print(f"    REQUEST: {round(time.time() - calling, 4)}  DATA: {str(response)}")
         except Exception as ex:
             print(f"ERROR tool: '{function_name}' args: {function_args}")
             pass
+
+
+# The final response
+print(response['message']['content'])
